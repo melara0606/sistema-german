20d5cadd89974fd5f3303316ceef4a01f842dca0
mariocardoza <mariokr.rocker@gmail.com>
2017-09-12 13:44:16 -0600

    español

--- app/Http/Requests/ModificarUsuarioRequest.php
+++ app/Http/Requests/ModificarUsuarioRequest.php
@@ -24,10 +24,24 @@ class ModificarUsuarioRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|string|max:255',
+            'name' => 'required|min:3|regex:/^([a-zA-ZñÑáéíóúÁÉÍÓÚ])+((\s*)+([a-zA-ZñÑáéíóúÁÉÍÓÚ]*)*)+$/|max:255',
             'username' => 'required|string|max:255',
             'email' => 'required|string|email|max:255',
             'cargo' => 'required',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+        'name.required'=>'El campo nombre obligatorio',
+        'name.regex'=>'El campo nombre debe contener solo texto',
+        'name.max'=>'El campo nombre no debe exceder 255 caracteres',
+        'name.min'=>'El campo nombre debe tener al menos 3 letras',
+        'email.unique'=>'El correo ya está en uso',
+        'username.required'=>'El campo nombre de usuario es obligatorio',
+        'username.min'=>'El nombre de usuario debe tener al menos 5 caracteres',
+        'username.unique'=>'El nombre de usuario ya está en uso',
         ];
     }
 }
