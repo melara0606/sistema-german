f89025ae09c17309c26c0a47a494a74a6b0450ee
mariocardoza <mariokr.rocker@gmail.com>
2017-11-07 14:04:31 -0600

    nueva pagina de error

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,4 +1,18 @@
 @extends('layouts.app')
 @section('content')
-<h1>No hemos podido encontrar lo que buscas</h1>
+    <div class="error-page">
+        <h2 class="headline text-yellow"> 404</h2>
+
+        <div class="error-content">
+            <h3><i class="fa fa-warning text-yellow"></i><b> Oops! Página no encontrada.</b></h3>
+
+            <p>
+            <h4>
+                No pudimos encontrar la página que buscas.
+                Talvez tú desees <a href="{{url('/home')}}">Volver a la página de inicio</a>
+            </h4>
+            </p>
+        </div>
+        <!-- /.error-content -->
+    </div>
 @endsection
