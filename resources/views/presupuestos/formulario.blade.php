aeb9fd586173a27f9508642b98b8e94edcdbe783
dianavelasquez <dya_re@hotmail.com>
2018-03-07 09:14:59 -0600

    corrigiendo

--- resources/views/presupuestos/formulario.blade.php
+++ resources/views/presupuestos/formulario.blade.php
@@ -16,15 +16,22 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-3 col-md-offset-3">
-                                <label for="" class="control-label">Item</label>
-                                {!!Form::number('',null,['class' => 'form-control','id' => 'item','min' => 1,'steps' => 1])!!}
-                            </div>
-                            <div class="col-md-3">
-                                <label for="" class="control-label">Categoría</label>
-                                {!! Form::text('',null,['class' => 'form-control','id' => 'categoria']) !!}
-                            </div>
-                      </div>
+                          <label for="" class="col-md-4 control-label">Seleccione Item</label>
+                          <div class="col-md-6">
+                            <select id="item" class="form-control">
+                                <option value="">Seleccione Item</option>   
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                          <label for="" class="col-md-4 control-label">Seleccione una categoría</label>
+                          <div class="col-md-6">
+                            <select id="categoria" class="form-control">
+                                <option value="">Seleccione categoría</option>   
+                            </select>
+                        </div>
+                    </div>
 
                       <div class="form-group">
                             <div class="col-md-12">
