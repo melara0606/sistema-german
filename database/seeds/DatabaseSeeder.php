ceb64079264dd2ea6cbbcfcf95f1ab5ccad06ce7
Edwin Melara <melara0606@gmail.com>
2018-07-20 17:25:05 -0600

    Update seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        //$this->call(ContribuyentesTableSeeder::class);
+        $this->call(ContribuyentesTableSeeder::class);
         $this->call(InmuebleTableSeeder::class);
     }
 }
